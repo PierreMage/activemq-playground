7f5175b7a26b1198fa53fc28d658d86aa458cb23
Pierre Mage <pierre.mage@gmail.com>
2012-10-12 18:56:05 +0100

    fill queue

--- src/main/java/ActiveMQPlayground.java
+++ src/main/java/ActiveMQPlayground.java
@@ -1,12 +1,27 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ActiveMQPlayground {
 
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(ActiveMQPlaygroundConfiguration.class);
-        QueueSender queueSender = context.getBean(QueueSender.class);
-        queueSender.send("Hello World!");
+        ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        fillQueue(context, threadPool, 1000 * 10);
         System.exit(0);
+    }
+
+    private static void fillQueue(final ApplicationContext context, ExecutorService threadPool, int millis) {
+        long aMinuteFromNow = System.currentTimeMillis() + millis;
+        while (System.currentTimeMillis() < aMinuteFromNow) {
+            threadPool.execute(new Runnable() {
+                public void run() {
+                    QueueSender queueSender = context.getBean(QueueSender.class);
+                    queueSender.send("Hello World!");
+                }
+            });
+        }
     }
 }
